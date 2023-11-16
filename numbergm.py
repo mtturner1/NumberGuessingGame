e3e4a4739ce943ce72c06e9e6f07a696229b7286
Zachery Whittaker (Class of 2024) <zachery.whittaker@salesianum.orgm>
2023-11-16 11:58:44 -0500

    final

--- numbergm.py
+++ numbergm.py
@@ -1,24 +1,19 @@
 import random
 def main(): 
-    number = generate_random_number()
-    guess = players_guess()
-    feedback = give_the_player_feedback(number, guess)
-    response = playAgain(feedback)
+    rndm_nmbr_gm()
     
 
-def generate_random_number(): 
+def rndm_nmbr_gm(): 
     return random.randint (1,100) #If you need to adjust the range feel free to do so
   
-def players_guess():
     return int (input("Player enter a guess"))
-def give_the_player_feedback(random_correct_number, guess):
+    
     if guess < random_correct_number: 
         return "Response is too low, please try again"
     elif guess > random_correct_number: 
         return "Response is too high, please try again"
     elif guess == random_correct_number:
      return "Response is correct!"
-
 
 def playAgain(): 
     playAgain = input("do you want to play again? ")
@@ -29,4 +24,5 @@ def playAgain():
         print("Bye!")
         
 if __name__ == "__main__":
-    main()
+  main() 
+   
